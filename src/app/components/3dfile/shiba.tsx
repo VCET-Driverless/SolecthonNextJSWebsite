2ba03f505bd6b545a4d0d5ec46abaff7046f7aa0
Pandey Adarsh Manoj <adarsh.224386101@vcet.edu.in>
2025-01-10 08:03:30 +0000

    updated

--- src/app/components/3dfile/shiba.tsx
+++ src/app/components/3dfile/shiba.tsx
@@ -2,12 +2,12 @@
 
 import { useRef } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { Mesh } from "three";
 
 function MeshComponent() {
-  const fileUrl = "/carmodel/scene.gltf";
+  const fileUrl = "/carmodel/scene.glb";  // put here name of the file 
   const mesh = useRef<Mesh>(null!);
   const gltf = useLoader(GLTFLoader, fileUrl);
 
@@ -16,7 +16,7 @@ function MeshComponent() {
   });
 
   return (
-    <mesh ref={mesh}  scale={[2, 2, 2]} position={[0, -1, 0]} // Move it slightly down
+    <mesh ref={mesh}  scale={[0.5,0.5, 0.5]} position={[0, -1, 0]} // Move it slightly down
     >
       <primitive object={gltf.scene} />
     </mesh>
@@ -25,12 +25,14 @@ function MeshComponent() {
 
 export function Shiba() {
   return (
-    <div className='flex justify-center items-center h-screen bg-black '>
-      <Canvas className='h-2xl w-2xl'>
+    <div className='flex justify-center items-center h-screen bg-white '>
+      <Canvas className='h-2xl w-2xl'
+      camera={{ position: [0, 3, 7], fov: 50 }}>
         <OrbitControls  enableZoom={false}/>
-        <ambientLight />  
+        <ambientLight color={"#CF0A0A"} intensity={2}/>  
         <pointLight position={[10, 10, 10]} />
         <MeshComponent />
+        <Environment preset="sunset" resolution={256}/> 
       </Canvas>
     </div>
   );
